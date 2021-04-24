2207590c88c73eeeeb34a726d9ae183723449c3c
Somebody4545 <>
2021-04-24 04:02:17 +0000

    blog posts and more!

--- script.js
+++ script.js
@@ -4,6 +4,6 @@
                 scrollPercent;
         $(window).unbind("scroll").scroll(function () {
             scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
-            body.style.backgroundPosition = "0px " + "-" + scrollPercent + "px";
+            body.style.backgroundPosition = "0px " + scrollPercent + "px";
         });
 })();
